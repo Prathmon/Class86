480eb2d090c8e10f37ba617b2a0dc3e65cc35b03
Prathmon <75729562+Prathmon@users.noreply.github.com>
2021-08-19 16:32:08 +0530

    Add files via upload

---
+++ class-85/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCBaivbfH9Tv9SYC-VuJrf5KSYSx7mllQ8",
+    authDomain: "storyteller-51ab2.firebaseapp.com",
+    projectId: "storyteller-51ab2",
+    storageBucket: "storyteller-51ab2.appspot.com",
+    messagingSenderId: "1028011909270",
+    appId: "1:1028011909270:web:ab62faa63745bb3dbce2b7",
+    measurementId: "G-6MLNJ753ET"
+  };  
